578e81da910475f4631773e583f6ecde81451d88
Vanderlândio Z. da Rocha <vandodev@gmail.com>
2022-07-17 13:28:52 -0300

    Normalizando porta do servidor

--- projeto/server.js
+++ projeto/server.js
@@ -5,13 +5,13 @@ const debug  = require('debug')('nodestr:server');
 const express = require('express');
 
 const app = express();
-const port = 4000;
+const port = normalizePort(process.env.PORT || '3000');
 app.set ('port', port)
 
 const server = http.createServer(app);
 const router = express.Router();
 
-
+//Rotas criadas
 const route = router.get('/', (req, res, next)=>{
     res.status(200).send({
         title: "Node Store API ",
@@ -21,6 +21,20 @@ const route = router.get('/', (req, res, next)=>{
 
 app.use('/', route);
 
+//Abrindo um servidor
 server.listen(port);
 console.log('Servidor rodando na porta: ' + port)
 
+
+//Normalizando a porta
+function normalizePort(val){
+    const port = parseInt(val, 10)
+    if(isNaN(port)){
+        return val;
+    }
+
+    if(port >= 0){
+        return port;
+    }
+    return false
+}
